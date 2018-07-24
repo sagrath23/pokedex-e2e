79e55283de9e0a1e2248b487f7fee9d43fc3870a
Steven Auguto Villegas Castellanos <steven@Multivac-2.0>
2018-07-24 01:23:11 -0500

    dependencies added

---
+++ src/PokemonList/PokemonList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class PokemonList extends Component {
+    render() {
+        return (
+            <ListGroup>
+                <ListGroupItem>Cras justo odio</ListGroupItem>
+                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                <ListGroupItem>Morbi leo risus</ListGroupItem>
+                <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                <ListGroupItem>Vestibulum at eros</ListGroupItem>
+            </ListGroup>
+        );
+    }
+}
---
+++ src/PokemonList/index.js
@@ -0,0 +1,5 @@
+import PokemonList from './PokemonList';
+
+export {
+    PokemonList
+}
