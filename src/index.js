f288af0514cd602a7748fbc955e720259b6f832f
Steven Auguto Villegas Castellanos <steven@Multivac-2.0>
2018-07-24 23:21:19 -0500

    provide redux store to app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import configureStore from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={configureStore()}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,20 @@
-import { createStore } from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from '../reducers/rootReducer';
+import rootSaga from '../sagas';
+import createSagaMiddleware from 'redux-saga'
 
-const history = createHistory();
+
+const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
+    const store = createStore(
+        rootReducer,
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+        applyMiddleware(sagaMiddleware)
+    );
+
+    sagaMiddleware.run(rootSaga);
+
+    return store;
+}
+
+export default configureStore;
