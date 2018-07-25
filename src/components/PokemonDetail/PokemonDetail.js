0fd4b390af2ad73cfff2e6eb99eb1743bf573fbb
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-07-25 14:45:10 -0500

    add saga to load pokemon's detail

--- src/components/PokemonDetail/PokemonDetail.js
+++ src/components/PokemonDetail/PokemonDetail.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     Card,
     CardImg,
-    CardText,
     CardBody,
     CardTitle,
     CardSubtitle,
@@ -12,23 +11,55 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+/*
 const PokemonDetail = ({detailedPokemon}) => (
     <Card>
         <CardImg top width="100%" src={detailedPokemon.sprite} alt="Pokemon Sprite" />
         <CardBody>
             <CardTitle>{detailedPokemon.name}</CardTitle>
             <CardSubtitle>Pokemon ID: {detailedPokemon.id}</CardSubtitle>
-            <CardText>
-                <ListGroup>
-                    {detailedPokemon.types.map((pokemonSlot) => (<ListGroupItem>{pokemonSlot.type.name}</ListGroupItem>))}
-                </ListGroup>
-            </CardText>
+            <ListGroup>
+                {detailedPokemon.types.map((pokemonSlot, index) => (<ListGroupItem key={index}>{pokemonSlot.type.name}</ListGroupItem>))}
+            </ListGroup>
             <Link to="/">
                 <Button>Back to list</Button>
             </Link>
-            
         </CardBody>
     </Card>
 );
+
+const PokemonDetail = (props) => {
+    console.log(props, 'detail props!!!');
+    return (<div>Hi</div>)
+}
+*/
+class PokemonDetail extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props, 'Holi');
+        // dispatch action to load new pokemon info
+        props.loadDetailAction(props.match.params.pokemonName);
+    }
+
+    render(){
+        const {detailedPokemon} = this.props;
+
+        return(
+            <Card>
+                <CardImg top width="100%" src={detailedPokemon.sprite} alt="Pokemon Sprite" />
+                <CardBody>
+                    <CardTitle>{detailedPokemon.name}</CardTitle>
+                    <CardSubtitle>Pokemon ID: {detailedPokemon.id}</CardSubtitle>
+                    <ListGroup>
+                        {detailedPokemon.types.map((pokemonSlot, index) => (<ListGroupItem key={index}>{pokemonSlot.type.name}</ListGroupItem>))}
+                    </ListGroup>
+                    <Link to="/">
+                        <Button>Back to list</Button>
+                    </Link>
+                </CardBody>
+            </Card>
+        );
+    }
+}
 
 export default PokemonDetail;
--- src/components/PokemonDetail/PokemonDetailContainer.js
+++ src/components/PokemonDetail/PokemonDetailContainer.js
@@ -1,18 +1,14 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import PokemonDetail from './PokemonDetail';
 import actions from '../../actions';
 
-/*
+
 const mapStateToProps = ({ pokedexReducer }) => ({
     detailedPokemon: pokedexReducer.detailedPokemon
 });
-*/
-const mapStateToProps = ({ pokedexReducer }) => {
-    console.log(pokedexReducer, 'Ahhhhhhhhhhhhh');
-    return { detailedPokemon: pokedexReducer.detailedPokemon };
-}
 
 const mapDispatchToProps = (dispatch) => (bindActionCreators(actions, dispatch));
 
-export default connect(mapStateToProps, mapDispatchToProps)(PokemonDetail);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PokemonDetail));
