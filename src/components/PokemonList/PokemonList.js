ee4867cfe0e3a57031b8e7b4a3fbe68e0962093c
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-07-24 19:04:13 -0500

    add buttons to pass pokemonList

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Col, Button, ButtonGroup, ListGroup, ListGroupItem } from 'reactstrap';
 
 export default class PokemonList extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            previousPage: '',
+            nextPage: ''
+        }
+    }
+
     render() {
         return (
-            <ListGroup>
-                <ListGroupItem>Cras justo odio</ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Morbi leo risus</ListGroupItem>
-                <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-                <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
+            <Col>
+                <ListGroup>
+                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem>Morbi leo risus</ListGroupItem>
+                    <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
+                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+                <ButtonGroup>
+                    <Button>Prev</Button>
+                    <Button>Next</Button>
+                </ButtonGroup>
+            </Col>
         );
     }
 }
