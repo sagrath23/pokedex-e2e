bce0adfae2efcc113201db27e95a8007dd7c8e68
Steven Auguto Villegas Castellanos <steven@Multivac-2.0>
2018-07-25 01:55:02 -0500

    dispatch actions are triggered by PokemonList component

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Col, Button, ButtonGroup, ListGroup, ListGroupItem } from 'reactstrap';
 
-const PokemonList = ({ currentPokemonList }) => {
-    console.log(currentPokemonList, 'List!!!!');
+const PokemonList = ({ currentPokemonList, loadListAction }) => {
     return (
     <Col>
         <ListGroup>
-{currentPokemonList.results.map((pokemon, index) => (<ListGroupItem key={index}>{pokemon.name}</ListGroupItem>))}
+            {currentPokemonList.results.map((pokemon, index) => (<ListGroupItem key={index}>{pokemon.name}</ListGroupItem>))}
         </ListGroup>
         <ButtonGroup>
-            <Button>Prev</Button>
-            <Button>Next</Button>
+            <Button
+                disabled={!currentPokemonList.prev}
+                onClick={() => {
+                    currentPokemonList.prev ? loadListAction(currentPokemonList.prev) : null
+                }}
+            >
+                Previous
+            </Button>
+            <Button
+                disabled={!currentPokemonList.next}
+                onClick={() => {
+                    currentPokemonList.next ? loadListAction(currentPokemonList.next) : null
+                }}
+            >
+                Next
+            </Button>
         </ButtonGroup>
     </Col>);
 
--- src/components/PokemonList/PokemonListContainer.js
+++ src/components/PokemonList/PokemonListContainer.js
@@ -1,10 +1,12 @@
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PokemonList from './PokemonList';
+import actions from '../../actions';
 
 const mapStateToProps = ({ pokedexReducer }) => ({
     currentPokemonList: pokedexReducer.currentPokemonList
 });
 
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => (bindActionCreators(actions, dispatch));
 
 export default connect(mapStateToProps, mapDispatchToProps)(PokemonList);
