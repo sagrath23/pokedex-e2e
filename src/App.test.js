7fcf65d6b7a2b7bd5adb856fcefc16cc283d5c53
Steven Auguto Villegas Castellanos <steven@Multivac-2.0>
2018-07-26 00:07:09 -0500

    add more e2e tests

--- src/App.test.js
+++ src/App.test.js
@@ -13,22 +13,50 @@ describe('list page', () => {
   let browser
   let page
   
-  beforeAll(async () => { 
+  beforeEach(async () => { 
     browser = await puppeteer.launch(isDebugging());
     page = await browser.newPage();
     await page.goto('http://localhost:3000/');
     page.setViewport({ width: 500, height: 2400 });
   });
 
-  afterAll(() => {     
+  afterEach(() => {     
     if (isDebugging()) {         
       browser.close();    
     } 
   });
 
-  test('in first page, first pokemon is bulbasaur', async() => {
+  it('in first page, first pokemon is bulbasaur', async() => {
     const html = await page.$eval('.list-group-item:nth-child(1) > a', e => e.innerHTML);
     expect(html).toBe('bulbasaur');
-    browser.close();
   }, 16000);
+
+  it('create an screenshot of bulbasaur detail', async() => {
+    const html = await page.$eval('.list-group-item:nth-child(1) > a', e => e.innerHTML);
+    await page.click('.list-group-item:nth-child(1) > a');
+    await page.waitForSelector('div.card');
+    await page.screenshot({path: 'screenshot.png'});
+    expect(html).toBe('bulbasaur');
+  }, 16000);
+
+  it('after click 5 times next button, the last pokemon listed is staryu', async() => {
+    await page.click('div.btn-group > button:nth-child(2)');
+    await page.click('div.btn-group > button:nth-child(2)');
+    await page.click('div.btn-group > button:nth-child(2)');
+    await page.click('div.btn-group > button:nth-child(2)');
+    await page.click('div.btn-group > button:nth-child(2)');
+
+    const html = await page.$eval('.list-group-item:last-child > a', e => e.innerHTML);
+    expect(html).toBe('staryu');
+  }, 16000);
+
+  it('go to detailed info about pikachu', async() => {
+    await page.click('div.btn-group > button:nth-child(2)');
+    await page.click('.list-group-item:nth-child(5) > a');
+    await page.waitForSelector('div.card');
+
+    const html = await page.$eval('div.card-body > h6', e => e.innerHTML);
+    expect(html).toBe('Pokemon ID: 25');
+  }, 16000);
+
 })
