4cecce96d9f4c5bd60911f30b3d88dba524b5232
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-07-26 17:23:29 -0500

    remove unused imports

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,4 @@
-import { delay } from 'redux-saga';
-import { put, takeEvery, all } from 'redux-saga/effects';
+import { all } from 'redux-saga/effects';
 import { watchLoadNewPokemonList } from './PokemonListSagas';
 import { watchLoadPokemonDetail } from './PokemonDetailSagas';
 
